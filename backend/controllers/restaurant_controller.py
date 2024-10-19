e084f8e305de26b1c8d22a76b46363db52f67070
andrewguilas <AndrewKyleGuilas@gmail.com>
2024-10-19 18:29:57 -0400

    returned restaurant based on emotion

--- backend/controllers/restaurant_controller.py
+++ backend/controllers/restaurant_controller.py
@@ -1,6 +1,25 @@
+import json
+
+RESTAURANT_FILE = "backend/data/restaurants_emotions.json"
+MAX_RESTAURANTS = 3
+
 def get_restaurants(emotion):
-    # TODO: Generate a list of restaurants based on the emotion
+    # Load restaurants data from the JSON file
+    with open(RESTAURANT_FILE, 'r') as file:
+        data = json.load(file)
 
-    restaurants = {}
+    # Initialize a list to hold filtered restaurants
+    restaurants = []
 
-    return restaurants
+    # Filter restaurants based on the emotion
+    for restaurant in data:
+        if emotion in restaurant.get("emotions"):
+            restaurants.append(restaurant)
+
+    return restaurants[:MAX_RESTAURANTS]
+
+def __main__():
+    print(get_restaurants('Social'))
+
+if __name__ == '__main__':
+    __main__()
