12ec709fe83b2a38d98cb90a5358f61263735b4f
andrewguilas <AndrewKyleGuilas@gmail.com>
2024-10-20 10:15:25 -0400

    Reapply "logged more backend errors"
    This reverts commit 51f3ec863f7680013dc5d528d23360aeb00ae5e7.

--- backend/controllers/restaurant_controller.py
+++ backend/controllers/restaurant_controller.py
@@ -3,19 +3,25 @@ import random
 
 RESTAURANT_FILE = "backend/data/restaurants_with_emotions.json"
 MAX_RESTAURANTS = 1
+DEBUG = True
 
 def get_restaurants(emotion):
-    with open(RESTAURANT_FILE, 'r') as file:
-        data = json.load(file)
+    try:
+        with open(RESTAURANT_FILE, 'r') as file:
+            data = json.load(file)
 
-    restaurants = []
-    for restaurant in data:
-        if emotion in restaurant.get("emotions"):
-            restaurants.append(restaurant)
+        restaurants = []
+        for restaurant in data:
+            if emotion in restaurant.get("emotions"):
+                restaurants.append(restaurant)
 
-    random.shuffle(restaurants)
+        random.shuffle(restaurants)
 
-    return restaurants[:MAX_RESTAURANTS]
+        return restaurants[:MAX_RESTAURANTS]
+    except Exception as e:
+        if DEBUG:
+            print(f"error in get_restaurants: {e}")
+        return []
 
 def __main__():
     print(get_restaurants('Social'))
